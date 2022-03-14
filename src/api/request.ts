62d0660a9e7a42da4bfdb37726aeabd5574eb643
Cristian Perez <perez.cr@icloud.com>
2022-03-14 00:50:10 -0500

    Fix lint warnings.

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,7 +1,9 @@
 import { AxiosResponse, Method } from 'axios';
 import axios from './axios';
 
-function request<T>(method: Method, url: string, data?: any): Promise<AxiosResponse<T>> {
+function request<T>(method: Method,
+  url: string, data?: unknown):
+Promise<AxiosResponse<T>> {
   return axios.request<T>({ method, url, data });
 }
 
